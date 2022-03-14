a604f657ecada1cb0827b409539a5197a3018405
AnthonyLaw <yc-law1015@hotmail.com>
2022-03-14 21:58:17 +0800

    [client] task: add table component

---
+++ client/src/components/Table/index.js
@@ -0,0 +1,41 @@
+import './Table.scss';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Table = function ({ userList }) {
+    const renderItem = (users) => {
+        return users.data.map((user, key) => {
+          return(
+            <tr key={key}>
+              <td><a href={`https://twitter.com/${user.twitterName}`}>@{user.twitterName}</a></td>
+              <td><a href={`https://explorer.nemtool.com/#/s_account?account=${user.nemAddress}`}>{user.nemAddress}</a></td>
+              <td><a href={`https://symbol.fyi/accounts/${user.symbolAddress}`}>{user.symbolAddress}</a></td>
+            </tr>
+          )
+        })
+      }
+
+	return (
+		<table>
+        <thead>
+          <tr>
+            <th>Twitter</th>
+            <th>NEM</th>
+            <th>SYMBOL</th>
+          </tr>
+        </thead>
+          <tbody>
+            { userList.data.length > 0 ? renderItem(userList) : 'No data available' }
+          </tbody>
+      </table>
+	);
+};
+
+Table.propTypes = {
+    userList: PropTypes.exact({
+		data: PropTypes.array.isRequired,
+		success: PropTypes.bool.isRequired
+	}).isRequired,
+};
+
+export default Table;
